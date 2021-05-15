0cce1bdf6ef8e5faa86babd6c053f4f04981ee13
戛戛Happy <sleepmoon.jia@gmail.com>
2021-05-15 15:49:17 +0800

    add context

---
+++ context/withcancel/withcancel.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func sleepRandom_1(stopChan chan struct{}) {
+	i := 0
+	for {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("This is sleep Random 1: %d\n", i)
+
+		i++
+		if i == 5 {
+			fmt.Println("cancel sleep random 1")
+			stopChan <- struct{}{}
+			break
+		}
+	}
+}
+
+func sleepRandom_2(ctx context.Context) {
+	i := 0
+	for {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("This is sleep Random 2: %d\n", i)
+		i++
+
+		select {
+		case <-ctx.Done():
+			fmt.Printf("Why? %s\n", ctx.Err())
+			fmt.Println("cancel sleep random 2")
+			return
+		default:
+		}
+	}
+}
+
+func main() {
+
+	ctxParent, cancelParent := context.WithCancel(context.Background())
+	ctxChild, _ := context.WithCancel(ctxParent)
+
+	stopChan := make(chan struct{})
+
+	go sleepRandom_1(stopChan)
+	go sleepRandom_2(ctxChild)
+
+	select {
+	case <-stopChan:
+		fmt.Println("stopChan received")
+	}
+	cancelParent()
+
+	for {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Continue...")
+	}
+}
---
+++ context/withouttime/withouttime.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	// 用于手动撤消，这里没有使用。使用的是定时撤消
+	defer cancel()
+
+	go handle(ctx, 500*time.Millisecond)
+	select {
+	case <-ctx.Done():
+		fmt.Println("main", ctx.Err())
+	}
+}
+
+func handle(ctx context.Context, duration time.Duration) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("handle", ctx.Err())
+	case <-time.After(duration):
+		fmt.Println("process request with", duration)
+	}
+}
